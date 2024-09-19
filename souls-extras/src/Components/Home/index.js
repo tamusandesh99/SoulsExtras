356dc58ef869acce368126cbf240c3b54a5f0f6a
Sandy Gurung <sandy.gurung@tierpoint.com>
2024-09-18 22:14:06 -0400

    add: react component

---
+++ souls-extras/src/Components/Home/index.js
@@ -0,0 +1,206 @@
+import React from 'react'
+import './index.scss'
+
+const Home = () => {
+    return (
+        <div className="main-container">
+            <div className='top-sub-container'>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+                <div className='player-box NPT'>
+                    <div className='player-contents'>
+
+                    </div>
+
+                    <div className='player-name'>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div className='bottom-sub-container'>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+                <div className='dragable-players'>
+                    <div className='player-logo'>
+
+                    </div>
+                    <div className='player-name'>
+                        NPT
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
